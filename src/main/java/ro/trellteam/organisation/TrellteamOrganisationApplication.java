70b96f33993b9e4742ba8f412b7017e84c8d59de
Popescu Robert <robertpopescu1903@gmail.com>
2022-11-13 12:27:31 +0200

    Initial commit

---
+++ src/main/java/ro/trellteam/organisation/TrellteamOrganisationApplication.java
@@ -0,0 +1,13 @@
+package ro.trellteam.organisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrellteamOrganisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrellteamOrganisationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/trellteam/organisation/TrellteamOrganisationApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.trellteam.organisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrellteamOrganisationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
